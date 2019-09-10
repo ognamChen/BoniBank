5a043b5b5600011566854405557faa9e717e28e8
ognam <program.ognam@gmail.com>
2019-09-10 15:14:08 +0800

    --

--- wp-content/themes/bonibank/page-content/product.php
+++ wp-content/themes/bonibank/page-content/product.php
@@ -1,5 +1,5 @@
 <?php $content_type = "product"?>
-<section class="product-section">
+<section class="product-section" style="background:linear-gradient(180deg, #e9ecef, #fff);">
     <div class="container">
         <?php
             $args = array(
@@ -18,23 +18,35 @@
                 ?>
                 <!-- PHP repeat start  -->  
                 <div class="row">
-                    <div class="col-lg-4">
-                        <div class="product-img text-center">
-                            <img class="img-fluid" src="<?php echo $image ?>" alt="" srcset="">
+                    <?php if (!empty($image)) { ?>
+                        <div class="col-lg-5">
+                            <div class="product-img text-center">
+                                <img class="img-fluid" src="<?php echo $image ?>" alt="" srcset="">
+                            </div>
                         </div>
-                    </div>
-                    <div class="col-lg-8">
-                        <div class="content-header" style="margin-top:30px; margin-bottom:0px;">
-                            <p>
-                                <?php echo $post->post_title ?>
-                            </p>
-                        </div>
-                        <div class="product-content">
-                            <p>
+                        <div class="col-lg-7">
+                            <div class="content-header text-center" style="margin-bottom:0px;">
+                                <p>
+                                    <?php echo $post->post_title ?>
+                                </p>
+                            </div>
+                            <div class="product-content">
                                 <?php echo $post->post_content ?>
-                            </p>
+                            </div>
                         </div>
-                    </div>
+                    <?php } else { ?>
+                        <div class="col-lg-12">
+                            <div class="content-header text-center" style="margin-bottom:0px;">
+                                <p>
+                                    <?php echo $post->post_title ?>
+                                </p>
+                            </div>
+                            <div class="text-center">
+                                <?php echo $post->post_content ?>
+                            </div>
+                        </div>
+                    <?php } ?>
+                    
                 </div>
 			<?php }?>
 			<!-- PHP repeat end  --> 
